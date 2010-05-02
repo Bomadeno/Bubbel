0136e8749452734d018cff0f0966e25bdf131b19
Matt <2095637+Bomadeno@users.noreply.github.com>
2010-05-02 20:34:19 +0000

    The game itself, at last, but without the project file just yet... needs fixing

---
+++ code/Bubbel Shot/Bubbel Shot/BubbelException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bubbel_Shot
+{
+    class BubbelException : ApplicationException
+    {
+        public BubbelException(string exception)
+            : base (exception)
+        {
+            
+        }
+    }
+}
