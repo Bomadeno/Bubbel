f3f8ee5537907653c9af9fe870e3a86bdfd39f88
bomadeno <bomadeno@d9cd9eec-ac47-43d4-a264-79fc7adea689>
2010-05-02 19:49:36 +0000

    The button library, used by the clickable menu, and by the game itself (probably)

---
+++ trunk/code/MattsButtonLibrary/MattsButtonLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MattsButtonLibrary")]
+[assembly: AssemblyProduct("MattsButtonLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("BomaCorp")]
+[assembly: AssemblyCopyright("Copyright © BomaCorp 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fba0d568-d0de-407a-be00-a7ee94f25ae0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
